903c02396a81fbb9fc38d18bf066dd518b7be07c
SidraDarmawan <agil77249@gmail.com>
2024-06-26 21:03:26 +0700

    first init

---
+++ firstCode.js
@@ -0,0 +1,33 @@
+console.log("Hello World");
+
+// this is comment
+
+/*
+this is
+multiline comment
+*/
+
+let message
+message = "Hello"
+
+let kata = "abc"
+
+console.log(kata)
+
+var globalvariable = "Hello World"
+const constant = "Purwadhika"
+
+kata = "def"
+console.log(kata)
+
+let a = 10
+var b = 20
+
+{
+let a = 30
+var b = 40
+}
+
+console.log(a)
+console.log(b)
+
